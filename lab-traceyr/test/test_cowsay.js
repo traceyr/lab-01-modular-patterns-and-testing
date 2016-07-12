29bc068afc55b540e69a3470069a17c2118b5225
Tracey <radcliffe.tracey@gmail.com>
2016-07-12 11:25:17 -0700

    made small change in test

--- lab-traceyr/test/test_cowsay.js
+++ lab-traceyr/test/test_cowsay.js
@@ -3,6 +3,6 @@ const cowsays = require('../lib/cowsay').Animal;
 
 describe('cows repeating a string', function() {
   it('should match between the two strings', function(){
-    expect(cowsays.cowsay('hello')).to.eql('The cow says "hello"');
+    expect(cowsays.cowsay('hello!')).to.eql('The cow says "hello!"');
   });
 });
