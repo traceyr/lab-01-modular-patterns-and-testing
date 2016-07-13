288f4b4710613ee8308cc486a1346c1072f79ec5
Tracey <radcliffe.tracey@gmail.com>
2016-07-13 11:15:28 -0700

    linting the gulp file

--- lab-traceyr/gulpfile.js
+++ lab-traceyr/gulpfile.js
@@ -4,6 +4,7 @@ const mocha = require('gulp-mocha');
 
 var testFiles = ['test/**/*.js'];
 var appFiles = ['lib/**/*.js'];
+var gulpFiles = ['gulp.js'];
 
 gulp.task('lint:app', () => {
   gulp.src(appFiles)
@@ -69,9 +70,41 @@ gulp.task('lint:test', () => {
   .pipe(eslint.format());
 });
 
+gulp.task('lint:gulp', () => {
+  gulp.src(gulpFiles)
+.pipe(eslint({
+  rules: {
+    'no-console': 0,
+    'indent': [2, 2],
+    'quotes': [2, 'single'],
+    'linebreak-style': [2, 'unix'],      'semi': [2,'always']
+  },
+  envs: [
+    'node',
+    'es6',
+    'browser'
+  ],
+  globals: [
+    'describe',
+    'it',
+    'beforeEach',
+    'afterEach',
+    'before',
+    'after'
+  ],
+  ecmaFeatures: {
+    'modules': true,
+    'experimentalObjectRestSpread': true,
+    'impliedStrict': true
+  },
+  extends: 'eslint:recommended'
+}))
+  .pipe(eslint.format());
+});
+
 gulp.task('mocha:test', () => {
   gulp.src(testFiles)
   .pipe(mocha());
 });
 
-gulp.task('default', ['lint:app', 'lint:test', 'mocha:test']);
+gulp.task('default', ['lint:app', 'lint:test', 'lint:gulp', 'mocha:test']);
