6ce32556f0e112eb4ff956658c73d4c7871070ed
Tracey <radcliffe.tracey@gmail.com>
2016-07-13 14:09:23 -0700

    fixed linting issues and a file name

--- lab-traceyr/gulpfile.js
+++ lab-traceyr/gulpfile.js
@@ -4,37 +4,37 @@ const mocha = require('gulp-mocha');
 
 var testFiles = ['test/**/*.js'];
 var appFiles = ['lib/**/*.js'];
-var gulpFiles = ['gulp.js'];
+var gulpFiles = ['gulpfile.js'];
 
 gulp.task('lint:app', () => {
   gulp.src(appFiles)
-.pipe(eslint({
-  rules: {
-    'no-console': 0,
-    'indent': [2, 2],
-    'quotes': [2, 'single'],
-    'linebreak-style': [2, 'unix'],      'semi': [2,'always']
-  },
-  envs: [
-    'node',
-    'es6',
-    'browser'
-  ],
-  globals: [
-    'describe',
-    'it',
-    'beforeEach',
-    'afterEach',
-    'before',
-    'after'
-  ],
-  ecmaFeatures: {
-    'modules': true,
-    'experimentalObjectRestSpread': true,
-    'impliedStrict': true
-  },
-  extends: 'eslint:recommended'
-}))
+  .pipe(eslint({
+    rules: {
+      'no-console': 0,
+      'indent': [2, 2],
+      'quotes': [2, 'single'],
+      'linebreak-style': [2, 'unix'],      'semi': [2,'always']
+    },
+    envs: [
+      'node',
+      'es6',
+      'browser'
+    ],
+    globals: [
+      'describe',
+      'it',
+      'beforeEach',
+      'afterEach',
+      'before',
+      'after'
+    ],
+    ecmaFeatures: {
+      'modules': true,
+      'experimentalObjectRestSpread': true,
+      'impliedStrict': true
+    },
+    extends: 'eslint:recommended'
+  }))
   .pipe(eslint.format());
 });
 
@@ -72,33 +72,33 @@ gulp.task('lint:test', () => {
 
 gulp.task('lint:gulp', () => {
   gulp.src(gulpFiles)
-.pipe(eslint({
-  rules: {
-    'no-console': 0,
-    'indent': [2, 2],
-    'quotes': [2, 'single'],
-    'linebreak-style': [2, 'unix'],      'semi': [2,'always']
-  },
-  envs: [
-    'node',
-    'es6',
-    'browser'
-  ],
-  globals: [
-    'describe',
-    'it',
-    'beforeEach',
-    'afterEach',
-    'before',
-    'after'
-  ],
-  ecmaFeatures: {
-    'modules': true,
-    'experimentalObjectRestSpread': true,
-    'impliedStrict': true
-  },
-  extends: 'eslint:recommended'
-}))
+  .pipe(eslint({
+    rules: {
+      'no-console': 0,
+      'indent': [2, 2],
+      'quotes': [2, 'single'],
+      'linebreak-style': [2, 'unix'],      'semi': [2,'always']
+    },
+    envs: [
+      'node',
+      'es6',
+      'browser'
+    ],
+    globals: [
+      'describe',
+      'it',
+      'beforeEach',
+      'afterEach',
+      'before',
+      'after'
+    ],
+    ecmaFeatures: {
+      'modules': true,
+      'experimentalObjectRestSpread': true,
+      'impliedStrict': true
+    },
+    extends: 'eslint:recommended'
+  }))
   .pipe(eslint.format());
 });
 
